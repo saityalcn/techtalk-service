e266a7c3684918a53da28617e5adc2e3d190a404
saityalcn <stsaityalcin@gmail.com>
2023-04-09 14:38:38 +0300

    add prepared model

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
+from data_analysis import getMostCommonPatterns, getLeastCommonPatterns, getMostCommonResponses, getLeastCommonResponses
+from chatgui import chatbot_response
 
 app = Flask(__name__)
 
@@ -15,10 +17,30 @@ def indexRoute():
 @app.route("/chat", methods=['POST'])
 @cross_origin()
 def chatRoute():
-    print(request.json)
-    return  {'res': chatFunc("abcabc")}
+    res = chatbot_response(request.json['message'])
+    return  {'res': res}
 
 
+@app.route("/analytics", methods=['POST'])
+@cross_origin()
+def analyticsRoute():
+    print(request.json['dataSource'])
+    dataSource = request.json['dataSource']
+
+    if(dataSource == 'mostCommonResponse'):
+        return {'res': getMostCommonResponses()}
+
+    elif(dataSource == 'leastCommonResponse'):
+        return {'res': getLeastCommonResponses()}
+
+    elif(dataSource == 'mostCommonPatterns'):
+        return {'res': getMostCommonPatterns()}
+
+    elif(dataSource == 'leastCommonPatterns'):
+        return {'res': getLeastCommonPatterns()}
+
+    else:
+        return {'res': []}
 
 def chatFunc(message): 
     return "test response message"
